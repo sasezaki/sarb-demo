9e3e0af2774ea1629028f77e60579ef326a3e15c
Dave Liddament <daveliddament@gmail.com>
2019-01-07 19:14:36 +0000

    Add code that creates more violations

--- src/Person.php
+++ src/Person.php
@@ -5,9 +5,24 @@ namespace Demo;
 class Person
 {
     /**
+     * @var int|null
+     */
+    private $age;
+
+    /**
      * @var string|null
      */
     private $name;
+
+    public function setAge(?int $age): void
+    {
+        $this->age = $age;
+    }
+
+    public function getAge(): int
+    {
+        return $this->age;
+    }
 
     public function setName(?string $name): void
     {
